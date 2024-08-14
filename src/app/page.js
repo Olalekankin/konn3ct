ed0db8d58596b86e82eded9dca1562e52b37052a
olalekankin <olalekankin@gmail.com>
2024-08-14 16:31:18 +0100

    participant error fixed

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import { useState } from 'react'
 import Modal from '@/components/Modal'
-import Participant from '@/components/participant'
+import Participant from '@/components/Participant'
 
 export default function Home() {
   const [isModalOpen, setModalOpen] = useState(false)
